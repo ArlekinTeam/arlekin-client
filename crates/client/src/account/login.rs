3b32de148cfe58bbd15f0b41aa7e81f5eced008d
Fivvee0 <mewaqin98@gmail.com>
2024-11-24 00:18:03 +0100

    Fixes

--- crates/client/src/account/login.rs
+++ crates/client/src/account/login.rs
@@ -1,40 +1,18 @@
+use crate::helpers::get_by_id::get_by_id;
 use crate::route::{Route, Router};
-use wasm_bindgen::JsCast;
-use web_sys::{console, HtmlInputElement};
+use web_sys::console;
 use yew::{function_component, html, Callback, Html};
 
 #[function_component(Login)]
 pub fn login() -> Html {
-    // Handle submit
     let onsubmit = Callback::from(move |e: yew::events::SubmitEvent| {
-        e.prevent_default(); // Prevent from something that is default
+        e.prevent_default();
 
-        // Access the DOM element
-        let email_input = web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .get_element_by_id("username")
-            .unwrap()
-            .dyn_into::<HtmlInputElement>()
-            .unwrap();
+        let email_input = get_by_id("email");
 
-        // Access the DOM element v2 (Too lazy to create a function for that)
-        let password_input = web_sys::window()
-            .unwrap()
-            .document()
-            .unwrap()
-            .get_element_by_id("password")
-            .unwrap()
-            .dyn_into::<HtmlInputElement>()
-            .unwrap();
+        let password_input = get_by_id("password");
 
-        // Get the values
-        let email_value = email_input.value();
-        let password_value = password_input.value();
-
-        // Log
-        console::log_1(&format!("Email: {}, Password: {}", email_value, password_value).into());
+        console::log_1(&format!("Email: {}, Password: {}", email_input, password_input).into());
     });
 
     html! {
@@ -42,9 +20,9 @@ pub fn login() -> Html {
         <Router route={Route::Login}/>
         <form onsubmit={onsubmit}>
             <input
-                placeholder="Username"
-                name="username"
-                id="username"
+                placeholder="Email"
+                name="email"
+                id="email"
                 type="text"
             />
             <input
--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -2,6 +2,7 @@ use app::AppInit;
 
 pub mod account;
 pub mod app;
+pub mod helpers;
 pub mod route;
 
 fn main() {
