cb9c85d901d4f3cce2b49f57457cc0228e467be4
Fivvee0 <mewaqin98@gmail.com>
2024-11-24 00:22:39 +0100

    fixing clippy errors

--- crates/client/src/helpers/get_by_id.rs
+++ crates/client/src/helpers/get_by_id.rs
@@ -2,14 +2,13 @@ use wasm_bindgen::JsCast;
 use web_sys::HtmlInputElement;
 
 pub fn get_by_id(id: &str) -> String {
-    let content = web_sys::window()
+    web_sys::window()
         .unwrap()
         .document()
         .unwrap()
-        .get_element_by_id(&id)
+        .get_element_by_id(id)
         .unwrap()
         .dyn_into::<HtmlInputElement>()
         .unwrap()
-        .value();
-    return content;
+        .value()
 }
