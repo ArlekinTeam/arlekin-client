718ccb6cf0809677a5259969769b5a81360a86f7
Fivvee0 <mewaqin98@gmail.com>
2024-11-25 20:54:31 +0100

    Fixing routes and some small changes

---
+++ crates/client/src/pages/not_found.rs
@@ -0,0 +1,10 @@
+use yew::{function_component, html, Html};
+
+#[function_component(NotFound)]
+pub fn not_found() -> Html {
+    html! {
+        <>
+        <h1>{ "404 - Not found" }</h1>
+        </>
+    }
+}
